680df6079c7c182689001e8b2ffdd12b56c42f81
DYLANCOLLINS23 <dlcollins@waimea.school.nz>
2025-03-19 12:07:25 +1300

    project setup
    project setup

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,8 @@
  * Programming Project for NCEA Level 3, Standard 91906
  * ---------------------------------------------------------------------
  * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Author: Dylan Collins
+ * GitHub Repo:    https://github.com/waimea-dlcollins/Level-3-programming-assessment#
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
